3e646f602d35686ce431e4ac8e0da250e8dd3f4e
Arturnix <artur.zgodka.programista@gmail.com>
2023-12-28 22:30:18 +0100

    Create login window view

---
+++ src/main/java/com/emailclientjavafx/emailclient/Launcher.java
@@ -0,0 +1,26 @@
+package com.emailclientjavafx.emailclient;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Launcher extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        Parent parent = FXMLLoader.load(getClass().getResource("view/LoginWindow.fxml"));
+        Scene scene = new Scene(parent, 451, 356);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
