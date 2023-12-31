4273f8f2f2cbef59d2bf70322ae74843019cddbc
Arturnix <artur.zgodka.programista@gmail.com>
2023-12-31 12:27:24 +0100

    Add javafx.web

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.emailclientjavafx.emailclient {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires javafx.web;
 
     opens com.emailclientjavafx.emailclient to javafx.fxml;
     exports com.emailclientjavafx.emailclient;
