b389172837079ca59955e469609dc3c74bd420d9
Arturnix <artur.zgodka.programista@gmail.com>
2024-01-09 16:17:06 +0100

    Add login data validation

--- src/main/java/com/emailclientjavafx/emailclient/controller/LoginWindowController.java
+++ src/main/java/com/emailclientjavafx/emailclient/controller/LoginWindowController.java
@@ -1,6 +1,8 @@
 package com.emailclientjavafx.emailclient.controller;
 
 import com.emailclientjavafx.emailclient.EmailManager;
+import com.emailclientjavafx.emailclient.controller.services.LoginService;
+import com.emailclientjavafx.emailclient.model.EmailAccount;
 import com.emailclientjavafx.emailclient.view.ViewFactory;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -26,10 +28,36 @@ public class LoginWindowController extends BaseController {
 
     @FXML
     void loginButtonAction() {
+
+        //if I have validation I can create email account/login
+        if(fieldsAreValid()) {
+            EmailAccount emailAccount = new EmailAccount(emailAddressField.getText(), passwordField.getText()); //getting login data from login window text fields
+            LoginService loginService = new LoginService(emailAccount, emailManager);
+            EmailLoginResult emailLoginResult = loginService.login();
+
+            switch (emailLoginResult) {
+                case SUCCESS:
+                    System.out.println("Login successfull! " + emailAccount);
+            }
+        }
         System.out.println("LoginButtonAction");
 
         viewFactory.showMainWindow();
         Stage stage = (Stage) errorLabel.getScene().getWindow();
         viewFactory.closeStage(stage);
+    }
+
+    private boolean fieldsAreValid() {
+
+        if(emailAddressField.getText().isEmpty()) {
+            errorLabel.setText("Please fill email");
+            return false;
+        }
+        if(passwordField.getText().isEmpty()) {
+            errorLabel.setText("Please fill password");
+            return false;
+        }
+
+        return true;
     }
 }
--- src/main/java/com/emailclientjavafx/emailclient/controller/services/LoginService.java
+++ src/main/java/com/emailclientjavafx/emailclient/controller/services/LoginService.java
@@ -2,7 +2,10 @@ package com.emailclientjavafx.emailclient.controller.services;
 //this package stores logic of email operations
 
 import com.emailclientjavafx.emailclient.EmailManager;
+import com.emailclientjavafx.emailclient.controller.EmailLoginResult;
 import com.emailclientjavafx.emailclient.model.EmailAccount;
+
+import javax.mail.*;
 
 public class LoginService {
 
@@ -14,8 +17,41 @@ public class LoginService {
         this.emailManager = emailManager;
     }
 
-    public void login() { //here will be only 1 method. Login method. But I must indicate the result of the login operation! Use enum to indicate login result.
+    public EmailLoginResult login() { //here will be only 1 method. Login method. But I must indicate the result of the login operation! Use enum to indicate login result.
 
+        Authenticator authenticator = new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(emailAccount.getAddress(), emailAccount.getPassword());
+            }
+        };
+
+        //from this point of program flow a lot of things can go wrong, so use try catch block
+        try {
+            //access the session
+            Session session = Session.getInstance(emailAccount.getProperties(), authenticator);
+            Store store = session.getStore("imaps");
+            //connect with this store
+            store.connect(emailAccount.getProperties().getProperty("incomingHost"),
+                    emailAccount.getAddress(),
+                    emailAccount.getPassword()); //put incoming host here
+            emailAccount.setStore(store); //using to get emails
+
+        } catch(javax.mail.NoSuchProviderException e) {
+            e.printStackTrace();
+            return EmailLoginResult.FAILED_BY_NETWORK;
+        } catch(AuthenticationFailedException e) {
+            e.printStackTrace();
+            return EmailLoginResult.FAILED_BY_CREDENTIALS;
+        } catch(MessagingException e) {
+            e.printStackTrace();
+            return EmailLoginResult.FAILED_BY_UNEXPECTED_ERROR;
+        } catch(Exception e) {
+            e.printStackTrace();
+            return EmailLoginResult.FAILED_BY_UNEXPECTED_ERROR;
+        }
+
+        return EmailLoginResult.SUCCESS;
 
     }
 }
