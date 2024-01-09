3567b702f60519bddcadbaf9ad1bccdbe9007f64
Arturnix <artur.zgodka.programista@gmail.com>
2024-01-09 11:38:05 +0100

    Add java mail libs

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.emailclientjavafx.emailclient {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires java.mail;
+    requires activation;
 
     opens com.emailclientjavafx.emailclient to javafx.fxml;
     exports com.emailclientjavafx.emailclient;
