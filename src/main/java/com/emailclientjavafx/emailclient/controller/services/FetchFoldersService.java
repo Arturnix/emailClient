92160a474a481984c8a1c707f13e219eff1a5b0a
Arturnix <artur.zgodka.programista@gmail.com>
2024-01-16 22:26:34 +0100

    Fetch email messages - show in console (subjects)

--- src/main/java/com/emailclientjavafx/emailclient/controller/services/FetchFoldersService.java
+++ src/main/java/com/emailclientjavafx/emailclient/controller/services/FetchFoldersService.java
@@ -35,6 +35,7 @@ public class FetchFoldersService extends Service<Void> { //as return type of Ser
         handleFolders(folders, foldersRoot);
     }
 
+    //get list of folders from email account
     private void handleFolders(Folder[] folders, EmailTreeItem<String> foldersRoot) throws MessagingException {
         //iterate through the folders list
         for(Folder folder : folders) {
@@ -44,6 +45,7 @@ public class FetchFoldersService extends Service<Void> { //as return type of Ser
             foldersRoot.getChildren().add(emailTreeItem);
             //list all the folders from email account to be visibile in email tree view as expanded
             foldersRoot.setExpanded(true);
+            fetchMessagesOnFolder(folder, emailTreeItem); //get email messages for each folder.
             if(folder.getType() == Folder.HOLDS_FOLDERS) { //czy wewnatrz tego folderu sa inne foldery
                 Folder[] subFolders = folder.list();
                 handleFolders(subFolders, emailTreeItem); //recursive
@@ -51,5 +53,26 @@ public class FetchFoldersService extends Service<Void> { //as return type of Ser
         }
     }
 
+    private void fetchMessagesOnFolder(Folder folder, EmailTreeItem<String> emailTreeItem) { //to get messages for folders, I will create separate service for each folder.
 
+        Service fetchMessagesService = new Service() { //dont need argument here because it doesnt return anything
+            @Override
+            protected Task createTask() {
+                return new Task() {
+                    @Override
+                    protected Object call() throws Exception { //here I start write my getting email logic
+                        if(folder.getType() != Folder.HOLDS_FOLDERS) {
+                            folder.open(Folder.READ_WRITE); //permissions read write
+                            int folderSize = folder.getMessageCount();
+                            for(int i = folderSize; i > 0; i--) {
+                                System.out.println(folder.getMessage(i).getSubject());
+                            }
+                        }
+                        return null;
+                    }
+                };
+            }
+        };
+        fetchMessagesService.start();
+    }
 }
