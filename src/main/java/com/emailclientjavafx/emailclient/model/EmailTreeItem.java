d9f13848ff9c272b254a3f41546c1a4385170649
Arturnix <artur.zgodka.programista@gmail.com>
2024-01-16 23:38:02 +0100

    Add messages to the folders

--- src/main/java/com/emailclientjavafx/emailclient/model/EmailTreeItem.java
+++ src/main/java/com/emailclientjavafx/emailclient/model/EmailTreeItem.java
@@ -1,14 +1,41 @@
 package com.emailclientjavafx.emailclient.model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.TreeItem;
+
+import javax.mail.Flags;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 public class EmailTreeItem<String> extends TreeItem<String> {
 
     private String name;
+    private ObservableList<EmailMessage> emailMessages;
 
     //I will use this tree item instead of regular tree item
     public EmailTreeItem(String name) {
         super(name);
         this.name = name;
+        this.emailMessages = FXCollections.observableArrayList(); //ObservableList is an interface so in the constructor I need helper class FXCollections. It generates list.
     }
+
+    //method to get messages and use in java fx program.
+    public void addEmail(Message message) throws MessagingException {
+        boolean messageIsRead = message.getFlags().contains(Flags.Flag.SEEN);
+        //construct email message
+        EmailMessage emailMessage = new EmailMessage(
+                message.getSubject(),
+                message.getFrom()[0].toString(),
+                message.getRecipients(MimeMessage.RecipientType.TO)[0].toString(),
+                message.getSize(),
+                message.getSentDate(),
+                messageIsRead,
+                message
+        );
+        emailMessages.add(emailMessage);
+        System.out.println("added to " + name + " " + message.getSubject());
+    }
+
 }
