0148853e3ba2a5daddcbbdcad968056a7ed1f33b
Arturnix <artur.zgodka.programista@gmail.com>
2024-01-27 11:54:45 +0100

    Added attachments buttons download behaviour

--- src/main/java/com/emailclientjavafx/emailclient/controller/EmailDetailsController.java
+++ src/main/java/com/emailclientjavafx/emailclient/controller/EmailDetailsController.java
@@ -4,6 +4,8 @@ import com.emailclientjavafx.emailclient.EmailManager;
 import com.emailclientjavafx.emailclient.controller.services.MessageRendererService;
 import com.emailclientjavafx.emailclient.model.EmailMessage;
 import com.emailclientjavafx.emailclient.view.ViewFactory;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -56,9 +58,9 @@ public class EmailDetailsController extends BaseController implements Initializa
     private void loadAttachemnts(EmailMessage emailMessage) {
         if(emailMessage.hasAttachments()) {
             for(MimeBodyPart mimeBodyPart : emailMessage.getAttachmentsList()) {
-                Button button = null;
+                AttachmentButton button = null;
                 try {
-                    button = new Button(mimeBodyPart.getFileName());
+                    button = new AttachmentButton(mimeBodyPart);
                 } catch (MessagingException e) {
                     throw new RuntimeException(e);
                 }
@@ -66,6 +68,37 @@ public class EmailDetailsController extends BaseController implements Initializa
             }
         } else {
             attachmentLabel.setText(""); //hide attchamnets field method if there is no attachment in email
+        }
+    }
+
+    private class AttachmentButton extends Button {
+
+        private MimeBodyPart mimeBodyPart;
+        private String downloadedFilePath;
+
+        public AttachmentButton(MimeBodyPart mimeBodyPart) throws MessagingException {
+            this.mimeBodyPart = mimeBodyPart;
+            this.setText(mimeBodyPart.getFileName());
+            this.downloadedFilePath = LOCATION_OF_DOWNLOADS + mimeBodyPart.getFileName();
+
+            this.setOnAction(e -> downloadAttachment());
+        }
+
+        private void downloadAttachment() { //it can be long going task so it is good idea to put it into another thread
+
+            Service service = new Service() {
+                @Override
+                protected Task createTask() {
+                    return new Task() {
+                        @Override
+                        protected Object call() throws Exception {
+                            mimeBodyPart.saveFile(downloadedFilePath);
+                            return null;
+                        }
+                    };
+                }
+            };
+            service.restart();
         }
     }
 }
